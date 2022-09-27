9412e4e7eabdf70ee6765adf4ae79d7d33758be3
sairam <sairam@sairam.kali>
2022-09-27 13:18:40 +0530

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
-
+import {CgMenuRight} from 'react-icons/cg'
 const Navbar = () => {
     const [sticky, setSticky] =  useState(false);
+    const [open, setOpen] =  useState(false);
     const menuLinks = [
         { name: 'HOME', link: '#home' },
         { name: 'ABOUT', link: '#about' },
@@ -19,12 +20,26 @@ const Navbar = () => {
         <nav className={`fixed w-full left-0 top-0 z-[999] ${sticky ? "md:bg-white/60 text-gray-900" : "text-white"}`}>
             <div className='flex item-center justify-between'>
                 <div className='mx-7'>
-                    <h4 className='text-4xl uppercase font-bold'>
+                    <h4 className='text-4xl uppercase font-bold m-3'>
                         Sairam<span className='text-orange-400 font-sans'> Kaushik</span>
                     </h4>
                 </div>
                 <div className={` ${sticky ? 'md:bg-white/0 bg-white' : 'bg-white'} text-gray-900 md:block hidden px-7 py-2 font-medium rounded-bl-full`}>
                     <ul className='flex items-center gap-1 py-2 text-lg'>
+                        {
+                            menuLinks?.map((menu,i) =>(
+                                <li key={i} className='px-6 hover:text-cyan-600'>
+                                    <a href={menu?.link}>{menu?.name}</a>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                <div className={`z-[999] text-gray-900 text-3xl md:hidden m-5`}>
+                    <CgMenuRight />
+                </div>
+                <div className='md:hidden text-gray-900 absolute w-2/3 h-screen px-7 py-2 font-medium bg-white top-0 right-0'>
+                    <ul className='flex flex-col justify-center h-full gap-10 py-2 text-lg'>
                         {
                             menuLinks?.map((menu,i) =>(
                                 <li key={i} className='px-6 hover:text-cyan-600'>
