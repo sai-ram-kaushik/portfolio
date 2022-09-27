1ccbdc570fd97ed43adbbb21ead2472b8cf0d940
sairam kaushik <sairamkaushik1@gmail.com>
2022-09-27 21:57:38 +0530

    added about section

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import Main from './components/Main';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
         <Navbar />
         <Hero />
-        <Main />
+        <About />
     </div>
   );
 }
