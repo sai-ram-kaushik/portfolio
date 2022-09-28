38752bed28b1c8899b912a76e94290e566e0023e
sairam kaushik <sairamkaushik1@gmail.com>
2022-09-28 21:20:56 +0530

    added projects and contact page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <About />
         <Skills />
         <Projects />
+        <Contact />
+        <Footer />
     </div>
   );
 }
