5c3514959f346d660dff3e7cee3bd09ab9d887f3
Sairam Kaushik <sairamkaushik1@gmail.com>
2022-12-07 10:49:49 +0530

    updated the project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,58 +1,71 @@
-import React, { useEffect } from 'react'
-import {AiOutlineArrowRight} from 'react-icons/ai'
+import React, { useEffect } from "react";
 import Aos from "aos";
-import 'aos/dist/aos.css';
+import "aos/dist/aos.css";
+import project1 from "../assests/images/aiccit.png";
+import project2 from "../assests/images/weather.png";
 const Projects = () => {
-
-  useEffect(() =>{
-    Aos.init({duration: 1000});
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
   }, []);
 
   return (
-    <section id='projects' className='py-10 relative'>
-      <div className='mt-8 text-gray-100 text-center'>
-        <h3 className='text-4xl font-semibold ' data-aos="fade-right">
-          My <span className='text-cyan-600'>Projects</span>
+    <section id="projects" className="py-10 relative">
+      <div className="mt-8 text-gray-100 text-center">
+        <h3 className="text-4xl font-semibold " data-aos="fade-right">
+          My <span className="text-cyan-600">Projects</span>
         </h3>
-        <p className='text-gray-400 mt-3 text-lg' data-aos="fade-right">My awesome works</p>
-        <div className=' flex justify-center mt-12'>
-          <div className='w-[30%] flex flex-wrap md:item-center justify-center' data-aos="fade-up">
-            <ul>
-              <li className='relative flex gap-6 pb-5 items-baseline'>
-                <div className='before:absolute before:left-[5.5px] before:h-full before:w-[1px] before:bg-gray-400'>
-                  <svg xmlns='http://wwww.w3.org/2000/svg' width="12" height="12" className='bi bi-circle fill-gray-400'>
-                    <circle cx="5" cy="5" r='10' />
-                  </svg>
-                </div>
-                <div className='text-lg text-gray-400'>
-                  <p className='text-3xl font-sans font-bold text-white'>AICCIT - 2023</p>
-                  <p className='mt-2 flex items-baseline'><AiOutlineArrowRight className='w-16 mt-2'/>A Website made with Reactjs and Tailwind CSS 
-                </p>
-                <p className='mt-2 flex items-baseline'><AiOutlineArrowRight className='w-16 mt-2'/>A Website for a college conference which will be integrated in our college official website
-                </p>
-                <p className='mt-2 flex items-baseline'><AiOutlineArrowRight className='w-16 mt-2'/>Check out the website -<span className='text-blue-500'><a href="https://aiccit2023.vercel.app/" target="_blank">Link</a></span></p>
-                </div>
-              </li>
+        <p className="text-gray-400 mt-3 text-lg" data-aos="fade-right">
+          My awesome works
+        </p>
+        <div className=" flex justify-center mt-12 gap-6">
+          <div
+            className="flex items-center justify-center mt-12  flex-row flex-wrap gap-10 "
+            data-aos="fade-up"
+          >
+            <div className="border-2 border-cyan-600 rounded-xl p-4 text-xl w-[35rem] font-poppins flex flex-col gap-5 justify-center bg-gray-900">
+              <div className="flex justify-start items-center">
+                <img src={project1} className="rounded-lg" />
+              </div>
 
-              <li className='relative flex gap-6 pb-5 items-baseline'>
-                <div className='before:absolute before:left-[5.5px] before:h-full before:w-[1px] before:bg-gray-400'>
-                  <svg xmlns='http://wwww.w3.org/2000/svg' width="12" height="12" className='bi bi-circle fill-gray-400'>
-                    <circle cx="5" cy="5" r='10' />
-                  </svg>
-                </div>
-                <div className='text-lg text-gray-400'>
-                  <p className='text-3xl font-sans font-bold text-white'>Weather Application</p>
-                  <p className='mt-2 flex items-baseline'><AiOutlineArrowRight className='w-16'/>A website made with Reactjs, Tailwind CSS and Weather API</p>
-                  <p className='mt-2 flex items-baseline'><AiOutlineArrowRight className='w-16'/>A platform where we can check the weather status of any country</p>
-                  <p className='mt-2 flex items-baseline'><AiOutlineArrowRight className='w-16'/>Check out the website - <span className='text-blue-500'><a href='https://weather-pi-khaki.vercel.app/'target="_blank">Link</a></span></p>
-                </div>
-              </li>
-            </ul>
+              <div className="flex flex-col font-bold leading-6 gap-3">
+                <h2 className="text-3xl">AICCIT-2023</h2>
+                <h2 className="text-xl">
+                  Website Link -{" "}
+                  <a
+                    href="https://aiccit2023.vercel.app/"
+                    target="_blank"
+                    className="text-blue-500"
+                  >
+                    Link
+                  </a>
+                </h2>
+              </div>
+            </div>
+
+            <div className="border-2 border-cyan-600 rounded-xl p-4 text-xl w-[35rem] font-poppins flex flex-col gap-5 justify-center bg-gray-900">
+              <div className="flex justify-start items-center">
+                <img src={project2} className="rounded-lg" />
+              </div>
+
+              <div className="flex flex-col font-bold leading-6 gap-3">
+                <h2 className="text-3xl">Weather Application</h2>
+                <h2 className="text-xl">
+                  Website Link -{" "}
+                  <a
+                    href="https://weather-pi-khaki.vercel.app/"
+                    target="_blank"
+                    className="text-blue-500"
+                  >
+                    Link
+                  </a>
+                </h2>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
